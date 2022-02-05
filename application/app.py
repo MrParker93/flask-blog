a497da7e32b650d5cb35723cebe83d362f2fce7b
Marcus Parker <marcuseparker93@gmail.com>
2022-02-05 17:10:48 +0000

    Setup app configurations and added dummy route to test server is working

---
+++ application/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+# Initializes the app and app configurations
+def create_app(config_setting):
+
+    app = Flask(__name__)
+
+    config_module = f'application.config.{config_setting.capitalize()}Config'
+
+    app.config.from_object(config_module)
+
+    @app.route('/')
+    def hello_world():
+        return "Hello, World!"
+
+    return app
